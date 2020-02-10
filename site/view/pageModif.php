7eab0c488090b0577ba650122c826a45901fd906
EGiorgisCPNV <48352389+EGiorgisCPNV@users.noreply.github.com>
2020-02-10 11:10:57 +0100

    Pour s'aligner avec mon camarade Dylan

--- site/view/pageModif.php
+++ site/view/pageModif.php
@@ -5,9 +5,17 @@
  * Initial version by: Esteban.GIORGIS
  * Initial version created on: 06.02.2020 13:47
  */
-
 ?>
-
-<form>
-
+<!-- ce formulaire sert a entrer les changements -->
+<form class="form" method='POST' action="site.php?action=login">
+    <label>Changement du pays</label>
+    <input type="text" name="pays" id="Pays">
+    <br>
+    <label>Changement de la ville</label>
+    <input type="text" name="ville" id="Ville">
+    <br>
+    <label>Changement de l'établissement</label>
+    <input type="text" name="etab" id="Etat">
+    <br>
+    <input type="submit" name="boutton" value="Enrgistrer">
 </form>
